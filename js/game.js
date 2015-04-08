0c5f31e5c52cb3bd19dbc566902c6ec12e769373
Collin hardy <Collin.Hardy25@gmail.com>
2015-04-08 11:08:06 -0700

    spending exp

--- js/game.js
+++ js/game.js
@@ -76,7 +76,7 @@ var game = {
             
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
-//                me.state.set(me.state.SPENDEXP, new game.spendExp());
+                me.state.set(me.state.SPENDEXP, new game.spendExp());
 
 		// Start the game.
 		me.state.change(me.state.MENU);
