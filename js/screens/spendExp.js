7074a2fc97a235e6207e6cc9f505733213db9f81
Collin hardy <Collin.Hardy25@gmail.com>
2015-04-09 10:40:10 -0700

    edit

--- js/screens/spendExp.js
+++ js/screens/spendExp.js
@@ -1,7 +1,27 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+game.SpendExp = me.ScreenObject.extend({
+    /**	
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
 
 
+        me.game.world.addChild(new (me.Renderable.extend({
+            init: function() {
+                this._super(me.Renderable, 'init', [380, 240, 300, 50]);
+                this.font = new me.Font("Helvetica", 46, "white");
+            },
+            draw: function(renderer) {
+                this.font.draw(renderer.getContext(), "SPEND", this.pos.x, this.pos.y);
+            }
+            
+        })));
+   },
+    /**	
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+
+    }
+});
